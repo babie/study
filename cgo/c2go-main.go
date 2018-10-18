c0e3c1865c9274b1198cb37d9ad9c045ba06a18d
babie <babie.wildheart@gmail.com>
2018-10-18 14:30:24 +0900

    refactor: Remove reflect package from imports.

--- cgo/c2go-main.go
+++ cgo/c2go-main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/babie/study/cgo/c2go"
 )
 
 func main() {
-	bt := c2go.GetTrue()
-	bf := c2go.GetFalse()
-	ni := c2go.GetInt()
-	nf := c2go.GetFloat()
-	nd := c2go.GetDouble()
-	fmt.Printf("GetTrue: %v %#v\n", reflect.TypeOf(bt), bt)
-	fmt.Printf("GetFalse: %v %#v\n", reflect.TypeOf(bf), bf)
-	fmt.Printf("GetInt: %v %#v\n", reflect.TypeOf(ni), ni)
-	fmt.Printf("GetFloat: %v %#v\n", reflect.TypeOf(nf), nf)
-	fmt.Printf("GetDouble: %v %#v\n", reflect.TypeOf(nd), nd)
+	fmt.Printf("GetTrue: %#v\n", c2go.GetTrue())
+	fmt.Printf("GetFalse: %#v\n", c2go.GetFalse())
+	fmt.Printf("GetInt: %#v\n", c2go.GetInt())
+	fmt.Printf("GetFloat: %#v\n", c2go.GetFloat())
+	fmt.Printf("GetDouble: %#v\n", c2go.GetDouble())
 }
